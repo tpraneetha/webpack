36e3b571b331bdb059873ad9c28b40ff3c2a69d4
praneetha tirupathi <tpraneetha@users.noreply.github.com>
2021-05-24 07:38:24 -0700

    2 commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,14 @@
-alert('from index.js/client');
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+console.log(checkForName);
+
+alert("I EXIST")
+export {
+    checkForName,
+    handleSubmit
+   }
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,12 @@
-function handleSubmit(event) {
+export function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
