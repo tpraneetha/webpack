f79f0528c3b050dc6d6261b69b566e9ed2dffc3b
praneetha tirupathi <tpraneetha@users.noreply.github.com>
2021-05-24 12:49:21 -0700

    7 commit

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,11 @@ const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin=require ("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+new WorkboxPlugin.GenerateSW()
 new CleanWebpackPlugin({
     // Simulate the removal of files
     dry: true,
@@ -14,10 +19,9 @@ new CleanWebpackPlugin({
 
 module.exports = {
     mode:"production",
-    entry: './src/client/index.js',output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
+    entry: './src/client/index.js',optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
 
 module: {
     rules: [
@@ -26,15 +30,15 @@ module: {
         exclude: /node_modules/,
         loader: "babel-loader"
             },{
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-        } 
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
     ]
 },
 plugins:[
     new HtmlWebPackPlugin({
         template:"./src/client/views/index.html",
         filename:"./index.html",
-    })
+    }),new MiniCssExtractPlugin({ filename: "[name].css" })
 ]
 }
