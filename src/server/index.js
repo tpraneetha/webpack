b2cbec44fde17bf731172887f9fd7f45e10f47ad
praneetha tirupathi <tpraneetha@users.noreply.github.com>
2021-05-24 11:39:56 -0700

    5 commit

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ app.use(express.static('src/client'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
