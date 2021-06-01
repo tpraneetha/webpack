84c55768342a07be7ca0358f460c20dce7d50321
praneetha tirupathi <tpraneetha@users.noreply.github.com>
2021-06-01 07:00:30 -0700

    corrections with test and babel

--- __tests__/formHandler.test.js
+++ __tests__/formHandler.test.js
@@ -1,5 +1,5 @@
 // import formHandler from '../src/client/js/formHandler';
 test('weatherGenerator() function', () => {
-  const formHandler=require('..src/client/js/formHandler')
+  const formHandler=require('../src/client/js/formHandler')
     expect(formHandler).toBeDefined();
   });
--- __tests__/validate.test.js
+++ __tests__/validate.test.js
@@ -1,5 +1,5 @@
 // import validate from '../src/client/js/validate';
 test('validate() function', () => {
-  const validate=require('..src/client/js/validate')
+  const validate=require('../src/client/js/validate')
     expect(validate).toBeDefined();
   });
