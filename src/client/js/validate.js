10571b0e4fd55ef4f1546a45b6a53cc14b11d5ae
praneetha tirupathi <tpraneetha@users.noreply.github.com>
2021-06-04 06:03:51 -0700

    changes to read.me

--- src/client/js/validate.js
+++ src/client/js/validate.js
@@ -2,5 +2,6 @@ export const validate = (url) => {
     var expression =
         /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     var regex = new RegExp(expression);
+   
     return regex.test(url);
 }
